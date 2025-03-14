2538ef657d4dd496833ddbf88a3a322cb29c755e
luiza2217 <luizakusma2@gmail.com>
2025-03-14 12:05:51 -0300

    trabalhando com entrada de dados com JOptionPane

--- Entrada_Joption.java
+++ Entrada_Joption.java
@@ -2,15 +2,15 @@ import javax.swing.JOptionPane;
 
 public class Entrada_Joption {
 public static void main(String[] args) {
-    String nome = JOptionPane.showInputDialog("Digite seu nome");
+    String nome = JOptionPane.showInputDialog("Digite seu nome"); //JOption é uma classe
 
    String idade = JOptionPane.showInputDialog("Digite a sua idade");
     
-  double altura = Double.parseDouble(JOptionPane.showInputDialog(" Digite a sua altura"));
+  double altura = Double.parseDouble(JOptionPane.showInputDialog(" Digite a sua altura")); //showInputDialog só aceita String
   
    int idadeStr = Integer.parseInt(idade); // Parse faz a conversao
 
-   JOptionPane.showMessageDialog(null, nome + idadeStr + altura);
+   JOptionPane.showMessageDialog(null, " Meu nome é " + nome + " Tenho " + idadeStr + " Minha altura é " + altura);
 
 }    
 
